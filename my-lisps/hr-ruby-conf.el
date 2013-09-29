82719740456ae19d0a5c07f27d8dcf837f3e5f93
daipeng <daipeng123456@gmail.com>
2013-09-29 14:35:18 +0800

    ruby-mode indent fix add markdown mode elpa emacs key add and fix

--- my-lisps/hr-ruby-conf.el
+++ my-lisps/hr-ruby-conf.el
@@ -2,6 +2,11 @@
 (setq ruby-indent-level 2)
 (setq nxml-child-inden 2)
 
+(defun ruby-indent-and-newline-change ()
+  "change the <C-j> and <RET> while on ruby mode"
+  (global-set-key (kbd "\C-j") 'autopair-newline)
+  (global-set-key (kbd "<RET>") 'reindent-then-newline-and-indent))
+(add-hook 'ruby-mode-hook 'ruby-indent-and-newline-change)
 
 ;; provide
 (provide 'hr-ruby-conf)
