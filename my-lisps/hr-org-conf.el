dcc0937947a37ca586f7acfe04feabab68fa35ee
hackrole <daipeng123456@gmail.com>
2016-04-08 19:03:29 +0800

    update org agent config

--- my-lisps/hr-org-conf.el
+++ my-lisps/hr-org-conf.el
@@ -2,7 +2,7 @@
 ;; TO SEE the hr-keys-conf
 
 ;; set org-agenda
-(setq org-agenda-files '("~/Desktop/work/work_plan.org"))
+(setq org-agenda-files '("~/.emacs.d/org-agent/work_plan.org"))
 
 ;; set keys
 (add-hook 'org-mode-hook (lambda ()
